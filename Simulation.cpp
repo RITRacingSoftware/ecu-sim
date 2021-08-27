190f77d4ba571e9698f8f50960fa8e20447c8db9
chblust <chblust98@gmail.com>
2021-08-27 19:02:26 +0000

    in progress

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,3 +1,5 @@
+#include <queue>
+
 #include "CanMessage.h"
 #include "Simulation.h"
 
@@ -12,18 +14,56 @@ void Simulation::tick()
 {
     this->message_matrix.clear();
 
+    // tick all the ecus and get all the can messages sent
     std::vector<Ecu*>::iterator ecu_iter;
-    for (ecu_iter = this->ecu_list.begin(); ecu_iter < this->ecu_list.end(); ecu_iter++)
+    for (ecu_iter = this->ecu_list.begin(); ecu_iter < this->ecu_list.end(); ++ecu_iter)
     {
         Ecu* ecu = *ecu_iter;
         ecu->tick();
-        this->message_matrix.push_back(ecu->empyCan());
+        this->message_matrix.push_back(ecu->emptyCan());
     }
 
-    std::vector<std::vector<CanMessage>>::iterator matrix_iter;
-    for (matrix_iter = this->message_matrix.begin(); matrix_iter < this->message_matrix.end(); message_matrix++)
+    // Figure out the most CAN messages any ECU sent
+    int i = 0;
+    int longest_matrix_len = 0;
+
+    std::vector<std::vector<CanMessage_s>*>::iterator matrix_iter;
+    for (matrix_iter = this->message_matrix.begin(); matrix_iter < this->message_matrix.end(); ++matrix_iter)
     {
-        // time for priority queue
+        if ((*matrix_iter)->size() > longest_matrix_len) longest_matrix_len = (*matrix_iter)->size();
+    }
+
+    // "Send" each CAN message to each ECU
+    for (int i = 0; i < longest_matrix_len; i++)
+    {
+        // sort by id
+        auto compare = [](CanMessage_s lhs, CanMessage_s rhs)
+        {
+            return lhs.id - rhs.id;
+        };
+
+        std::priority_queue<CanMessage_s, std::vector<CanMessage_s>, decltype(compare)> ordered_msgs(compare);
+        for (matrix_iter = this->message_matrix.begin(); matrix_iter < this->message_matrix.end(); ++matrix_iter) 
+        {
+            if ((*matrix_iter)->size() > i)
+            {
+                ordered_msgs.push((*matrix_iter)->at(i));
+            }
+        }
+
+        // Deliver CAN messages to ECUs in order
+        CanMessage_s next_msg;
+        while(ordered_msgs.size() > 0)
+        {
+            next_msg = ordered_msgs.top();
+            for (ecu_iter = this->ecu_list.begin(); ecu_iter < this->ecu_list.end(); ecu_iter++)
+            {
+                Ecu* ecu = *ecu_iter;
+                ecu->injectCan(next_msg);
+            }
+
+            ordered_msgs.pop();
+        }
     }
 
 
@@ -37,7 +77,7 @@ void Simulation::run_ms(int num_ticks)
 Simulation::~Simulation()
 {
     std::vector<Ecu*>::iterator ecu;
-    for (ecu = this->ecu_list.begin(); ecu < this->ecu_list.end(); ecu++)
+    for (ecu = this->ecu_list.begin(); ecu < this->ecu_list.end(); ++ecu)
     {
         delete *ecu;
     }
